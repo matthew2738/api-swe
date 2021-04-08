0ce2f340e14d0dbcdbd32783354e7554ea0473ed
matthew2738 <60441234+matthew2738@users.noreply.github.com>
2021-04-08 14:11:14 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,65 @@
+import json
+import requests
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    image = db.Column(db.String(100))
+    zip = db.Column(db.Integer)
+    city = db.Column(db.String(50))
+    category = db.Column(db.String(50))
+    postdate = db.Column(db.Integer)
+    startdate = db.Column(db.Integer)
+    enddate = db.Column(db.Integer)
+    description = db.Column(db.String(120))
+
+    def __repr__ (self):
+        return f"{self.title} - {self.description}"
+
+@app.route("/")
+def home():
+    return "Hello, This is the API for the SWE Class"
+
+@app.route('/posts')
+def get_posts():
+    posts = Post.query.all()
+    
+    output = []
+    for post in posts:
+        post_data = {'title': post.title, 'description': post.description}
+        output.append(post_data)
+    return {"posts" : output}
+
+@app.route("/posts/<id>")
+def get_post(id):
+    post = Post.query.get_or_404(id)
+    return {"title": post.title, "description": post.description}
+
+@app.route('/posts', methods=['POST'])
+def add_post():
+    post = Post(title=request.json['title'],
+                image=request.json['image'],
+                zip=request.json['zip'],
+                city=request.json['city'],
+                category=request.json['category'],
+                postdate=request.json['postdate'],
+                startdate=request.json['startdate'],
+                enddate=request.json['enddate'],
+                description=request.json['description'])
+    db.session.add(post)
+    db.session.commit()
+    return {'id': post.id}
+
+@app.route('/posts/<id>', methods=['DELETE'])
+def delete_post(id):
+    post = Post.query.get(id)
+    if post is None:
+        return {"error": "not found"}
+    db.session.delete(post)
+    db.session.commit()
+    return {"message": "Deleted"}
